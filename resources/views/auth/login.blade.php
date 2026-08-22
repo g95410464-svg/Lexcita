43e01a91091d50ce7d23fa624cc5f605100d1006
Guillermo Castillo <g95410464@gmail.com>
2026-08-21 18:31:52 -0600

    actualizacion de login diseño mas moderno correcion de botones

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -138,7 +138,8 @@
 <h2 class="font-headline-lg-mobile text-headline-lg-mobile text-on-surface font-semibold">Iniciar Sesión</h2>
 <p class="font-label-sm text-label-sm text-on-surface-variant mt-2">Bienvenido de nuevo.</p>
 </div>
-<form class="space-y-6">
+<form method="POST" action="{{ route('login') }}" class="space-y-6">
+    @csrf
 <!-- Email Input -->
 <div class="space-y-2">
 <label class="font-label-sm text-label-sm text-on-surface block" for="email">Correo Electrónico</label>
@@ -163,7 +164,7 @@
 </div>
 <!-- Remember Me -->
 <div class="flex items-center">
-<input class="h-4 w-4 bg-surface-container-high border-outline-variant text-primary focus:ring-primary focus:ring-offset-background rounded-full" id="remember-me" name="remember-me" type="checkbox">
+<input class="h-4 w-4 bg-surface-container-high border-outline-variant text-primary focus:ring-primary focus:ring-offset-background rounded-full" id="remember-me" name="remember" type="checkbox">
 <label class="ml-2 block font-label-sm text-label-sm text-on-surface-variant" for="remember-me">
                         Mantener sesión iniciada
                     </label>
@@ -184,15 +185,15 @@
 </div>
 <div class="mt-8">
 <!-- Secondary Action (Google) -->
-<button class="w-full bg-transparent border border-outline-variant hover:border-on-surface text-on-surface font-body-md py-3 transition-all duration-300 flex justify-center items-center gap-3 rounded-full" type="button">
+<a href="{{ route('google.redirect') }}" class="w-full bg-transparent border border-outline-variant hover:border-on-surface text-on-surface font-body-md py-3 transition-all duration-300 flex justify-center items-center gap-3 rounded-full">
 <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"></path>
 <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"></path>
 <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43 .35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22 .81-.62z" fill="#FBBC05"></path>
 <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.304 4.53 6.16-4.53z" fill="#EA4335"></path>
 </svg>
-<span class="">Continuar con Google</span>
-</button>
+<span>Continuar con Google</span>
+</a>
 </div>
 <div class="mt-8 text-center">
 <p class="font-label-sm text-label-sm text-on-surface-variant">
